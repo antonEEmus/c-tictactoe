bd97b48f995503a586f15bba02be8cde5032a1f5
Anton Karpov <anton.karpov@rossvideo.com>
2026-08-06 20:06:44 -0400

    Implemented working console renderer for windows

--- main.c
+++ main.c
@@ -1,40 +1,69 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "header.h"
+#include <string.h>
+#include <windows.h>
 
-AppState state = MENU;
+// void CreateConsole();
+// void WriteScreenToConsole(const char** screen, int width, int height);
+
+#define MAX_W 200
+#define MAX_H 200
+
+static HANDLE hConsole;
+static DWORD bytesWritten = 0;
+static COORD writeCoord = {0, 0};
+
+void CreateConsole() {
+  hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL,
+                                       CONSOLE_TEXTMODE_BUFFER, NULL);
+  SetConsoleActiveScreenBuffer(hConsole);
+}
+
+void WriteScreenToConsole(char screen[MAX_H][MAX_W], int width, int height) {
+  for (int y = 0; y < height; y++) {
+    WriteConsoleOutputCharacterA(hConsole, screen[y], width, (COORD){2, y + 2},
+                                 &bytesWritten);
+  }
+}
+
+const int screenWidth = 20;
+const int screenHeight = 16;
+const int screenSize = screenHeight * screenHeight;
 
 int main() {
-  while(1) {
-    switch (state) {
-      case GAME:
-        HandleGame();
-        break;
-      case PAUSE: 
-        HandlePause();
-        break;
-      case MENU:
-        HandleMenu();
-        break;
+  char screen[MAX_H][MAX_W];
+  for (int y = 0; y < screenHeight; y++) {
+    for (int x = 0; x < screenWidth; x++) {
+      screen[y][x] = ' ';
     }
   }
+
+  int count = 0;
+  size_t textSize = 12;
+  char text[textSize];
+  int textY = screenHeight / 2;
+  int textX = screenWidth / 2;
+  CreateConsole();
+  WriteScreenToConsole(screen, screenWidth, screenHeight);
+  while (1) {
+    _itoa_s(count, text, textSize, 10);
+    size_t textLen = strlen(text);
+
+    textX = screenWidth / 2 - textLen / 2;
+    for (int x = 0; x < screenWidth; x++) {
+      screen[textY][x] = ' ';
+    }
+    for (int i = 0, x = textX; i < textLen; i++) {
+      screen[textY][x] = text[i];
+      x++;
+    }
+
+    WriteScreenToConsole(screen, screenWidth, screenHeight);
+
+    count++;
+    Sleep(1000);
+  }
+
   return 0;
-}
-
-void HandleGame() {
-  clearScreen();
-  printf("Game handled...");
-}
-
-void HandlePause() {
-  clearScreen();
-  printf("Game paused...");
-}
-
-void HandleMenu() {
-  renderFrame(
-    "Welcome To The Menu\n"
-    "[Play]\n"
-    "[Exit]\n"
-  );
 }
